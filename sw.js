8bc3ab4050a743b9a1a5f898261f5a70b74c4c1c
tmiura27twm <t.miura27.twm@gmail.com>
2026-08-21 16:57:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 倉庫ツール Service Worker（アプリ化用）
 // データはApps Script（ネット必須）。ここではアプリの外枠だけキャッシュして起動を速くする。
-const CACHE = 'soko-v17';
+const CACHE = 'soko-v18';
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json', './zxing.min.js']).catch(() => {})));
